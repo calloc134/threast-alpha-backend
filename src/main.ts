f46682c7344702570006863b463b2eabbf3d6d54
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-04-18 23:43:20 +0900

    [update] Prismaモジュール追加

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { PrismaModule } from './submodules/prisma_mo/prisma.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
+    PrismaModule,
     new FastifyAdapter(),
   );
   await app.listen(3000);
